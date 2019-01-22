bcdb996e1125fa1c827bbba9ace6c32cc100ebe2
huangjizhong <1124831983@qq.com>
2019-01-22 16:13:51 +0800

    mydog新版本对应修改

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,12 +3,9 @@ import { map } from "./app/domain/map";
 
 let app = createApp();
 
-app.set("encodeDecodeConfig", { "encode": null, "decode": decode });
-
 let _map_instance: map = null as any
 
 app.configure("area", function () {
-    app.set("connectorConfig", { connector: "net", heartbeat: 6 });
     _map_instance = new map(app);
 });
 
@@ -19,11 +16,6 @@ app.start();
 process.on("uncaughtException", function (err: any) {
     console.log(err)
 })
-
-function decode(cmdId: number, msgBuf: Buffer): any {
-    console.log(new Date().toLocaleString(), app.routeConfig[cmdId]);
-    return JSON.parse(msgBuf as any);
-}
 
 
 export let map_instance = _map_instance
